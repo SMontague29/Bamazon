e614d90d2af20791d3da5e007fe729bbc3670049
Scott Montague <SMontague29@gmail.com>
2017-08-08 10:32:34 -0600

    clean up code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,34 +1,23 @@
-// The customer module is part of bamazon.
-// Users can view a list of products in bamazon.
-// And select to purchase products.
-
-// Required node modules.
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-// Connects to the database.
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
 
-  // Root is default username.
   user: "root",
   // Password is empty string.
   password: "",
   database: "Bamazon_db"
 });
 
-
-// If connection doesn't work, throws error, else...
 connection.connect(function(err) {
   if (err) throw err;
 
-  // Displays list of available products.
   displayProducts();
 
 });
 
-// Displays list of all available products.
 var displayProducts = function() {
 	var query = "Select * FROM products";
 	connection.query(query, function(err, res) {
@@ -40,12 +29,10 @@ var displayProducts = function() {
 						res[i].product_name + " || Price: " + res[i].price);
 		}
 
-		// Requests product and number of product items user wishes to purchase.
   		requestProduct();
 	});
 };
 
-// Requests product and number of product items user wishes to purchase.
 var requestProduct = function() {
 	inquirer.prompt([{
 		name: "productID",
@@ -69,7 +56,6 @@ var requestProduct = function() {
 		}
 	}]).then(function(answer) {
 
-		// Queries database for selected product.
 		var query = "Select stock_quantity, price, product_sales, department_name FROM products WHERE ?";
 		connection.query(query, { item_id: answer.productID}, function(err, res) {
 			
@@ -80,17 +66,13 @@ var requestProduct = function() {
 			var productSales = res[0].product_sales;
 			var productDepartment = res[0].department_name;
 
-			// Checks there's enough inventory  to process user's request.
 			if (available_stock >= answer.productUnits) {
 
-				// Processes user's request passing in data to complete purchase.
 				completePurchase(available_stock, price_per_unit, productSales, productDepartment, answer.productID, answer.productUnits);
 			} else {
 
-				// Tells user there isn't enough stock left.
 				console.log("There isn't enough stock left!");
 
-				// Lets user request a new product.
 				requestProduct();
 			}
 		});
@@ -98,19 +80,14 @@ var requestProduct = function() {
 };
 
 
-// Completes user's request to purchase product.
 var completePurchase = function(availableStock, price, productSales, productDepartment, selectedProductID, selectedProductUnits) {
-	
-	// Updates stock quantity once purchase complete.
+
 	var updatedStockQuantity = availableStock - selectedProductUnits;
 
-	// Calculates total price for purchase based on unit price, and number of units.
 	var totalPrice = price * selectedProductUnits;
 
-	// Updates total product sales.
 	var updatedProductSales = parseInt(productSales) + parseInt(totalPrice);
-	
-	// Updates stock quantity on the database based on user's purchase.
+
 	var query = "UPDATE products SET ? WHERE ?";
 	connection.query(query, [{
 		stock_quantity: updatedStockQuantity,
@@ -120,22 +97,17 @@ var completePurchase = function(availableStock, price, productSales, productDepa
 	}], function(err, res) {
 
 		if (err) throw err;
-		// Tells user purchase is a success.
+
 		console.log("Yay, your purchase is complete.");
 
-		// Display the total price for that purchase.
 		console.log("You're mythical payment has been received in the amount of : " + totalPrice);
 
-		// Updates department revenue based on purchase.
 		updateDepartmentRevenue(updatedProductSales, productDepartment);
-		// Displays products so user can make a new selection.
 	});
 };
 
-// Updates total sales for department after completed purchase.
 var updateDepartmentRevenue = function(updatedProductSales, productDepartment) {
 
-	// Query database for total sales value for department.
 	var query = "Select total_sales FROM departments WHERE ?";
 	connection.query(query, { department_name: productDepartment}, function(err, res) {
 
@@ -145,12 +117,10 @@ var updateDepartmentRevenue = function(updatedProductSales, productDepartment) {
 
 		var updatedDepartmentSales = parseInt(departmentSales) + parseInt(updatedProductSales);
 
-		// Completes update to total sales for department.
 		completeDepartmentSalesUpdate(updatedDepartmentSales, productDepartment);
 	});
 };
 
-// Completes update to total sales for department on database.
 var completeDepartmentSalesUpdate = function(updatedDepartmentSales, productDepartment) {
 
 	var query = "UPDATE departments SET ? WHERE ?";
@@ -162,7 +132,6 @@ var completeDepartmentSalesUpdate = function(updatedDepartmentSales, productDepa
 
 		if (err) throw err;
 
-		// Displays products so user can choose to make another purchase.
 		displayProducts();
 	});
 };
